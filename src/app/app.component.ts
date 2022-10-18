869137c7d8a54e3ab10a3d395d5663efe2d4ed43
justcoddev <edisonpaulcrz@gmail.com>
2022-10-18 12:20:19 -0500

    Directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivas';
+  // *ngFor
+  listEstudiantes: any[] = [
+    { nombre: 'Teddy Summers', estado: 'Promocionado' },
+    { nombre: 'Paul Mc', estado: 'Regular' },
+    { nombre: 'Cristina Marin', estado: 'Regular' }
+  ]
+  listEstudiantes2: any[] = [
+    // { nombre: 'Teddy Summers', estado: 'Promocionado' },
+    // { nombre: 'Paul Mc', estado: 'Regular' },
+    // { nombre: 'Cristina Marin', estado: 'Regular' }
+  ]
+  listEstudiantes3: any[] = [
+    { nombre: 'Teddy Summers', estado: 'Promocionado' },
+    { nombre: 'Paul Mc', estado: 'Regular' },
+    { nombre: 'Cristina Marin', estado: 'Regular' }
+  ]
+  listEstudiantes4: any[] = [
+    { nombre: 'Teddy Summers', estado: 'Promocionado' },
+    { nombre: 'Paul Mc', estado: 'Regular' },
+    { nombre: 'Cristina Marin', estado: 'Regular' },
+    { nombre: 'Veronica Gonzales', estado: 'Promocionado' }
+  ]
+  listEstudiantes5: any[] = [
+    { nombre: 'Teddy Summers', estado: 'Promocionado' },
+    { nombre: 'Paul Mc', estado: 'Regular' },
+    { nombre: 'Cristina Marin', estado: 'Libre' },
+    { nombre: 'Veronica Gonzales', estado: 'Promocionado' }
+  ]
+
+  // *ngIf
+  mostrar = true;
+  toogle(): void {
+    this.mostrar = !this.mostrar
+  }
+
+  // *ngStyle
+  // todo se hace en el HTML
+
+  // *ngClass
+  // todo se hace en el HTML
+  // *ngSwitch
+
 }
